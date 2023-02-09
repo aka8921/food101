fd5eb7a7b211d61c2b558ba5247cd0b8e002128d
vighnesh S <vighneshaka8921@gmail.com>
2023-02-09 13:45:32 +0530

    Add Menu

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,3 +1,6 @@
+import plus from "../assets/plus.svg"
+import minus from "../assets/minus.svg"
+
 export const Menu = () => {
     const menu = [
         {
@@ -11,26 +14,65 @@ export const Menu = () => {
             name: "Samosa",
             price: "$30.00",
             img: "https://www.indianhealthyrecipes.com/wp-content/uploads/2019/11/samosa-recipe.jpg"
+        },
+        {
+            _id: "1234",
+            name: "Veg Biriyani",
+            price: "$50.00",
+            img: "https://www.indianveggiedelight.com/wp-content/uploads/2020/04/veg-biryani-instant-pot.jpg"
+        },
+        {
+            _id: "1432",
+            name: "Samosa",
+            price: "$30.00",
+            img: "https://www.indianhealthyrecipes.com/wp-content/uploads/2019/11/samosa-recipe.jpg"
+        },
+        {
+            _id: "1234",
+            name: "Veg Biriyani",
+            price: "$50.00",
+            img: "https://www.indianveggiedelight.com/wp-content/uploads/2020/04/veg-biryani-instant-pot.jpg"
+        },
+        {
+            _id: "1432",
+            name: "Samosa",
+            price: "$30.00",
+            img: "https://www.indianhealthyrecipes.com/wp-content/uploads/2019/11/samosa-recipe.jpg"
         }
+        
     ]
 
-    const handleCart = (id) => {
+    const handleCartAdd = (id) => {
         alert(`Adding ${id} to Cart`)
+    }
+
+    const handleCartRemove = (id) => {
+        alert(`Removing ${id} to Cart`)
     }
 
 
     const menuItems = menu.map ((item, i) => {
         return(
-            <div className="flex flex-col items-center" key={i}>
+            <div className="flex flex-col items-center shrink-0" key={i}>
                 <div className="flex flex-col items-center relative">
                     <div className="bg-red-300 w-[70px] relative h-[70px] rounded-full overflow-hidden">
                         <img src={item.img} alt={item.name} className="w-full h-full object-cover"/>
                     </div>
-                    <div className="px-3 flex justify-between py-1 rounded-full bg-white mt-[-10px] relative z-50 font-semibold cursor-pointer text-[10px] shadow-lg mb-1"
+                    <div className="w-[80px] overflow-hidden h-[25px] flex justify-between items-center rounded-full bg-white mt-[-10px] relative z-50 font-semibold cursor-pointer text-[10px] shadow-lg mb-1"
                     >
-                        <div className="">+</div>
-                        <div className="">0</div>
-                        <div className="">-</div>
+                        <div 
+                        className="flex items-center self-stretch w-[25px] justify-center"
+                        onClick={() => {handleCartAdd(item._id)}}
+                        >
+                            <img src={plus} alt="" className="w-[20px]" />
+                        </div>
+                        <div className="">100</div>
+                        <div 
+                        className="flex items-center self-stretch w-[25px] justify-center"
+                        onClick={() => {handleCartRemove(item._id)}}
+                        >
+                            <img src={minus} alt="" className="w-[20px]" />
+                        </div>
                     </div>
                 </div>
                 <div className="flex flex-col items-center">
@@ -43,7 +85,7 @@ export const Menu = () => {
     return (
     <div className="flex flex-col my-2">
         <div className="font-bold text-lg mb-3">Recent Bookings</div>
-        <div className="flex gap-5">
+        <div className="flex gap-5 overflow-scroll">
         {menuItems}
         </div>
     </div>
